19bae8f3791741171f61f1381de966443638fd70
Farishine28 <chotirose.pra@gmail.com>
2021-10-06 17:32:41 +0700

    Revert "Initial Preprocessing"
    This reverts commit 15247e24ccfcad0ca847f6e4aaded5cb543d8c53.

--- Preprocessing.py
+++ Preprocessing.py
@@ -18,4 +18,3 @@ for i in year:
         for k in numf:
             dF = pd.read_csv('C:/Users/Choti/Documents/270702/Term Project/Data/Bangkok_'+j+'_'+i+'_'+k+'.csv')
             df = df.append(dF)
-            
